f63c94ba8dcfff42b9fa3a14e0bfe5d64d8b1648
nishkarsh dubb <nishdubb11@gmail.com>
2020-09-27 22:46:51 +0530

    first commit

---
+++ mail.js
@@ -0,0 +1,13 @@
+
+const mailgun = require("mailgun-js");
+const DOMAIN = 'YOUR_DOMAIN_NAME';
+const mg = mailgun({apiKey: 'xxxxxxxxYOUR_API_KEYxxxxxxxx', domain: 'xxxxxxxxYOUR_DOMAINxxxxxxxx'});
+const data = {
+	from: 'abc@gmail.com'//the sender's email address,
+	to: '123@gmail.com'//the reciever's email address,
+	subject: 'Hello',
+	text: 'Testing some Mailgun awesomness!'
+};
+mg.messages().send(data, function (error, body) {
+	console.log(body);
+});
