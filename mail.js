3cd6efa33e868b18fa94f49ca8fd76aabeb42d34
Nishkarsh01 <nishdubb11@gmail.com>
2020-10-21 08:08:35 +0530

    Update mail.js

--- mail.js
+++ mail.js
@@ -3,7 +3,7 @@ const mailgun = require("mailgun-js");
 const DOMAIN = 'YOUR_DOMAIN_NAME';
 const mg = mailgun({apiKey: 'xxxxxxxxYOUR_API_KEYxxxxxxxx', domain: 'xxxxxxxxYOUR_DOMAINxxxxxxxx'});
 const data = {
-	from: 'abc@gmail.com'//the sender's email address,
+	from: 'abc@gmail.com'//the sender's email address, //make sure that you authorize this recipient 
 	to: '123@gmail.com'//the reciever's email address,
 	subject: 'Hello',
 	text: 'Testing some Mailgun awesomness!'
